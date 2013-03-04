bac526122dac74cbf27144df8903b7b5946d0c83
Łukasz Langa <lukasz@langa.pl>
2013-03-04 22:52:27 +0100

    long overdue Python 3 support

--- setup.py
+++ setup.py
@@ -1,38 +1,47 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2010 Łukasz Langa
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3 of the License.
+# Copyright (C) 2010-2013 by Łukasz Langa
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import os
 import sys
+import codecs
 from setuptools import setup, find_packages
 
-reload(sys)
-sys.setdefaultencoding('utf8')
+PY3 = sys.version_info[0] == 3
 
-ld_file = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
-try:
+if not PY3:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+
+with codecs.open(
+    os.path.join(os.path.dirname(__file__), 'README.rst'), 'r', 'utf8',
+) as ld_file:
     long_description = ld_file.read()
-finally:
-    ld_file.close()
 # We let it die a horrible tracebacking death if reading the file fails.
 # We couldn't sensibly recover anyway: we need the long description.
 
 setup (
     name = 'dj',
-    version = '0.0',
+    version = '0.1',
     author = 'Łukasz Langa',
     author_email = 'lukasz@langa.pl',
     description = "This is a namespace package for extra Django packages. Feel free to use it.",
@@ -49,14 +58,15 @@ setup (
     install_requires = [
         'django',
     ],
-
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        ]
-    )
+    ]
+)
